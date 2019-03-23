6e86a8124b3b39399ed7598aaeb8c9994bd33c25
Marek Bachorski <m.bachorski@gmail.com>
2019-03-23 09:54:17 +0100

    initial commit of empty android project

---
+++ KotlinNativeSampleAndroid/app/src/main/java/pl/mbachorski/kotlin_native/MainActivity.kt
@@ -0,0 +1,12 @@
+package pl.mbachorski.kotlin_native
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
